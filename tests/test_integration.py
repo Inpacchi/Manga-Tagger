7a12ea149e58256f0851796a7aa17665665c6c28
Yovarni Yearwood <yyearwood@endlessgalaxy.dev>
2021-01-15 01:55:00 -0500

    New integration tests for comicinfo creation & series title verification

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from unittest.mock import patch
 
 from MangaTaggerLib.api import AniList
-from MangaTaggerLib.MangaTaggerLib import metadata_tagger
+from MangaTaggerLib.MangaTaggerLib import metadata_tagger, construct_comicinfo_xml
 from MangaTaggerLib.models import Metadata
 from tests.database import MetadataTable as MetadataTableTest
 
@@ -23,9 +23,9 @@ class TestMetadata(unittest.TestCase):
         AniList.initialize()
 
         patch1 = patch('MangaTaggerLib.models.AppSettings')
-        self.AppSettings = patch1.start()
+        self.models_AppSettings = patch1.start()
         self.addCleanup(patch1.stop)
-        self.AppSettings.timezone = 'America/New_York'
+        self.models_AppSettings.timezone = 'America/New_York'
 
         patch2 = patch('MangaTaggerLib.MangaTaggerLib.MetadataTable')
         self.MetadataTable = patch2.start()
@@ -38,9 +38,61 @@ class TestMetadata(unittest.TestCase):
         self.CURRENTLY_PENDING_DB_SEARCH = patch3.start()
         self.addCleanup(patch3.stop)
 
+        patch4 = patch('MangaTaggerLib.MangaTaggerLib.AppSettings')
+        self.MangaTaggerLib_AppSettings = patch4.start()
+        self.addCleanup(patch4.stop)
+
+    def test_comicinfo_xml_creation_case_1(self):
+        title = 'Absolute Boyfriend'
+
+        self.MangaTaggerLib_AppSettings.mode_settings = {}
+
+        with open(Path(self.data_dir, title, self.data_file), encoding='utf-8') as data:
+            jikan_details = json.load(data)
+
+        with open(Path(self.data_dir, title, self.staff_file), encoding='utf-8') as data:
+            anilist_details = json.load(data)
+
+        manga_metadata = Metadata(title, {}, jikan_details, anilist_details)
+
+        self.assertTrue(construct_comicinfo_xml(manga_metadata, '001', {}))
+
+    def test_comicinfo_xml_creation_case_2(self):
+        title = 'Peach Girl Next [EN]'
+
+        self.MangaTaggerLib_AppSettings.mode_settings = {}
+
+        with open(Path(self.data_dir, title, self.data_file), encoding='utf-8') as data:
+            jikan_details = json.load(data)
+
+        with open(Path(self.data_dir, title, self.staff_file), encoding='utf-8') as data:
+            anilist_details = json.load(data)
+
+        manga_metadata = Metadata(title, {}, jikan_details, anilist_details)
+
+        self.assertTrue(construct_comicinfo_xml(manga_metadata, '001', {}))
+
+    def test_comicinfo_xml_creation_case_3(self):
+        title = 'G-Maru Edition'
+
+        self.MangaTaggerLib_AppSettings.mode_settings = {}
+
+        with open(Path(self.data_dir, title, self.data_file), encoding='utf-8') as data:
+            jikan_details = json.load(data)
+
+        with open(Path(self.data_dir, title, self.staff_file), encoding='utf-8') as data:
+            anilist_details = json.load(data)
+
+        manga_metadata = Metadata(title, {}, jikan_details, anilist_details)
+
+        self.assertTrue(construct_comicinfo_xml(manga_metadata, '001', {}))
+
     def test_metadata_case_1(self):
         title = 'Absolute Boyfriend'
+
         self.CURRENTLY_PENDING_DB_SEARCH.append(title)
+
+        self.MangaTaggerLib_AppSettings.mode_settings = { 'write_comicinfo': False }
 
         with open(Path(self.data_dir, title, self.data_file), encoding='utf-8') as data:
             jikan_details = json.load(data)
@@ -55,7 +107,10 @@ class TestMetadata(unittest.TestCase):
 
     def test_metadata_case_2(self):
         title = 'Peach Girl Next [EN]'
+
         self.CURRENTLY_PENDING_DB_SEARCH.append(title)
+
+        self.MangaTaggerLib_AppSettings.mode_settings = { 'write_comicinfo': False }
 
         with open(Path(self.data_dir, title, self.data_file), encoding='utf-8') as data:
             jikan_details = json.load(data)
@@ -67,3 +122,41 @@ class TestMetadata(unittest.TestCase):
         actual_manga_metadata = metadata_tagger(title, '001', {})
 
         self.assertEqual(expected_manga_metadata.test_value(), actual_manga_metadata.test_value())
+
+    def test_metadata_case_3(self):
+        actual_title = 'G-Maru Edition'
+        downloaded_title = '(G) Edition'
+
+        self.CURRENTLY_PENDING_DB_SEARCH.append(downloaded_title)
+
+        self.MangaTaggerLib_AppSettings.mode_settings = { 'write_comicinfo': False }
+
+        with open(Path(self.data_dir, actual_title, self.data_file), encoding='utf-8') as data:
+            jikan_details = json.load(data)
+
+        with open(Path(self.data_dir, actual_title, self.staff_file), encoding='utf-8') as data:
+            anilist_details = json.load(data)
+
+        expected_manga_metadata = Metadata(actual_title, {}, jikan_details, anilist_details)
+        actual_manga_metadata = metadata_tagger(downloaded_title, '001', {})
+
+        self.assertEqual(expected_manga_metadata.test_value(), actual_manga_metadata.test_value())
+
+    def test_metadata_case_4(self):
+        actual_title = 'Absolute Boyfriend'
+        downloaded_title = 'Boyfriend'
+
+        self.CURRENTLY_PENDING_DB_SEARCH.append(downloaded_title)
+
+        self.MangaTaggerLib_AppSettings.mode_settings = { 'write_comicinfo': False }
+
+        with open(Path(self.data_dir, actual_title, self.data_file), encoding='utf-8') as data:
+            jikan_details = json.load(data)
+
+        with open(Path(self.data_dir, actual_title, self.staff_file), encoding='utf-8') as data:
+            anilist_details = json.load(data)
+
+        expected_manga_metadata = Metadata(actual_title, {}, jikan_details, anilist_details)
+        actual_manga_metadata = metadata_tagger(downloaded_title, '001', {})
+
+        self.assertNotEqual(expected_manga_metadata.test_value(), actual_manga_metadata.test_value())
